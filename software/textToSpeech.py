226afb37dfdd684b95e4259651671a011393c2db
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-07-03 19:58:43 +0930

    Voice Instructions for Text-to-Speech

--- software/textToSpeech.py
+++ software/textToSpeech.py
@@ -14,10 +14,13 @@ def play_audio(audio_bytes):
         check=True,
     )
 
+
+instructions = "Voice Affect: Energetic and animated; dynamic with variations in pitch and tone. Tone: Excited and enthusiastic, conveying an upbeat and thrilling atmosphere. Pacing: Rapid delivery when describing the game or the key moments (e.g., an overtime thriller, pull off an unbelievable win to convey the intensity and build excitement. Slightly slower during dramatic pauses to let key points sink in. Emotion: Intensely focused, and excited. Giving off positive energy. Personality: Relatable and engaging. Pauses: Short, purposeful pauses after key moments in the game."
 def text_to_speech(text):
     response = client.audio.speech.create(
         model="tts-1",
-        voice="shimmer",
+        voice="onyx",
+        instructions=instructions,
         input=text
     )
     audio_content = response.read()
